af9ac59cf2561361e3dbf7007e0d10fbd2368638
Marcus Buckland <alreadysignedin@gmail.com>
2021-12-02 12:48:55 +1300

    Changed some formatting in main.py

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     game = Game()
     round_counter = 1
     while not (game.is_finished()):
-        print(f"ROUND {round_counter}")
+        print(f"Round {round_counter}:")
         round_counter += 1
         game.play_round()
         print(game)
