985ac205f38c4639c32ee8f62eaa27b8dc2999d1
marcusbuckland <alreadysignedin@gmail.com>
2022-01-07 09:56:04 +1300

    Added get_dice_remaining_amount method

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         print("=============================")
         print(f"Round {round_counter}:")
         print("=============================")
+        print(f"There are {game.get_dice_remaining_amount()} dice remaining.\n")
         round_counter += 1
         game.play_round()
-        # print(game)
     print(f"{game.get_remaining_players()[0].get_name()} is the winner!!")
