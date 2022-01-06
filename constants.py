8be75af0101ca98c9d37723c2e24fefead0d162f
marcusbuckland <alreadysignedin@gmail.com>
2022-01-06 16:58:13 +1300

    Increased Quantity words in the Constants class

--- constants.py
+++ constants.py
@@ -1,5 +1,10 @@
 class Constants:
 
-    quantity_words = dict(zip([i for i in range(0, 11)], ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]))
+    quantity_words = dict(zip([i for i in range(0, 31)], ["Zero", "One", "Two", "Three", "Four", "Five", "Six",
+                                                          "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+                                                          "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+                                                          "Eighteen", "Nineteen", "Twenty", "Twenty-one", "Twenty-two",
+                                                          "Twenty-three", "Twenty-four", "Twenty-five", "Twenty-six",
+                                                          "Twenty-seven", "Twenty-eight", "Twenty-nine", "Thirty"]))
     dice_words = dict(zip([i for i in range(1, 7)], ["ones", "twos", "threes", "fours", "fives", "sixes"]))
     singular_dice_words = dict(zip([i for i in range(1, 7)], ["one", "two", "three", "four", "five", "six"]))
