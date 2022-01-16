8107b50729388d4f700d0c5134a31a609d9fea1e
marcusbuckland <alreadysignedin@gmail.com>
2022-01-17 11:18:15 +1300

    Game now has a Godmode meaning you can see each players dice at the start of a round.

--- __main__.py
+++ __main__.py
@@ -1,12 +1,14 @@
 from game import Game
 
 if __name__ == '__main__':
-    game = Game(['Marcus','Bede', 'Steph', 'Kim'])
+    game = Game(player_names=['Marcus','Bede', 'Steph', 'Kim'], god_mode=True)
     round_counter = 1
     while game.not_finished():
         print("=============================")
         print(f"Round {round_counter}:")
         print("=============================")
+        if game.is_god_mode():
+            game.print_state()
         print(f"There are {game.get_dice_remaining_amount()} dice remaining.\n")
         round_counter += 1
         game.play_round()
