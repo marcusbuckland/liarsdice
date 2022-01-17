dd96fc535c1cb4b84adfe6803573dddfc226c614
marcusbuckland <alreadysignedin@gmail.com>
2022-01-18 09:40:50 +1300

    Changed VALID_BID_VALUES from list to set and capitalised variable names.

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ class Constants:
     NOT_ACE_PROBABILITY = 1/3 # The probability of rolling {2, 3, 4, 5, 6} on a 6 sided die when 1's are wild.
     DICE_START_AMOUNT = 5
 
-    quantity_words = dict(zip([i for i in range(0, 51)], ["Zero", "One", "Two", "Three", "Four", "Five", "Six",
+    QUANTITY_WORDS = dict(zip([i for i in range(0, 51)], ["Zero", "One", "Two", "Three", "Four", "Five", "Six",
                                                           "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
                                                           "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
                                                           "Eighteen", "Nineteen", "Twenty", "Twenty-one", "Twenty-two",
@@ -16,18 +16,17 @@ class Constants:
                                                           "Thirty-nine", "Forty", "Forty-one", "Forty-two", "Forty-three",
                                                           "Forty-four", "Forty-five", "Forty-six", "Forty-seven", "Forty-eight",
                                                           "Forty-nine", "Fifty"]))
+    DICE_WORDS_PLURAL = dict(zip([i for i in range(1, 7)], ["ones", "twos", "threes", "fours", "fives", "sixes"]))
+    DICE_WORDS_SINGULAR = dict(zip([i for i in range(1, 7)], ["one", "two", "three", "four", "five", "six"]))
 
-    dice_words = dict(zip([i for i in range(1, 7)], ["ones", "twos", "threes", "fours", "fives", "sixes"]))
-    singular_dice_words = dict(zip([i for i in range(1, 7)], ["one", "two", "three", "four", "five", "six"]))
+    VALID_RESPONSES = {"bid", "b", "call", "c", "exactcall", "e", "quit", "q"}
+    BID_RESPONSES = {"bid", "b"}
+    CALL_RESPONSES = {"call", "c"}
+    EXACTCALL_RESPONSES = {"exactcall", "e"}
+    QUIT_RESPONSES = {"quit", "q"}
+    VALID_BID_VALUES = set(range(1, 7))
 
-    valid_responses = ["bid", "b", "call", "c", "exactcall", "e", "quit", "q"]
-    bid_responses = ["bid", "b"]
-    call_responses = ["call", "c"]
-    exactcall_responses = ["exactcall", "e"]
-    quit_responses = ["quit", "q"]
-    valid_bid_values = [i for i in range(1,7)]
-
-    game_title = """
+    GAME_TITLE = """
   _____     _____       _       _______     ______    ______   _____   ______  ________  
  |_   _|   |_   _|     / \     |_   __ \  .' ____ \  |_   _ `.|_   _|.' ___  ||_   __  | 
    | |       | |      / _ \      | |__) | | (___ \_|   | | `. \ | | / .'   \_|  | |_ \_| 
