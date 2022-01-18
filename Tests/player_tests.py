be7ec7b42e9078368dedd7d78df3b66e355fe6d9
marcusbuckland <alreadysignedin@gmail.com>
2022-01-19 10:04:32 +1300

    Added a test.

--- Tests/player_tests.py
+++ Tests/player_tests.py
@@ -4,7 +4,7 @@ from player import is_valid_value
 
 
 class PlayerTests(unittest.TestCase):
-    def test_erroneous_bid_values(self):
+    def test_erroneous_bids(self):
         bid_value = 7
         self.assertFalse(is_valid_value(bid_value))
 
@@ -25,6 +25,11 @@ class PlayerTests(unittest.TestCase):
 
         bid_value = math.inf ** math.inf
         self.assertFalse(is_valid_value(bid_value))
+
+    def test_valid_bid_values(self):
+        for bid_value in range(1, 7):
+            self.assertTrue(is_valid_value(bid_value))
+
 
 
 if __name__ == '__main__':
