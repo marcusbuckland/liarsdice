160d2397985c601b0838fac72b0f8a2c29c8bd44
marcusbuckland <alreadysignedin@gmail.com>
2022-01-19 15:04:49 +1300

    Die constructor takes value as argument.

--- die.py
+++ die.py
@@ -4,8 +4,8 @@ import random
 class Die:
     """ Represents a die object."""
 
-    def __init__(self):
-        self.value = random.randint(1, 6)
+    def __init__(self, value=None):
+        self.value = random.randint(1, 6) if value is None else value
 
     def __repr__(self):
         return str(self.value)
